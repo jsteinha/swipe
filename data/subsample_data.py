30b0c3c687053bb28494cdf716177b9bcc99dbcb
strin <stl501@gmail.com>
2014-07-16 10:15:35 -0700

    toy data using only 3-character words

---
+++ data/subsample_data.py
@@ -0,0 +1,16 @@
+import os
+
+data = file('train2.dat').readlines()
+unique = dict()
+output = list()
+for line in data:
+  word = line.split(' ')[0]
+  if len(word) <= 3:
+    print word
+    output.append(word)
+    unique[word] = 1
+print 'length = ', len(output)
+print 'unique = ', len(unique.keys())
+output_file = open('train2_small.dat', 'w')
+[output_file.write(line) for line in output]
+output_file.close()
