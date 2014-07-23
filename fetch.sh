a07dd4002a1a2434997e0c328d3f213d231e252e
strin <stl501@gmail.com>
2014-07-23 10:51:53 -0700

    .

--- fetch.sh
+++ fetch.sh
@@ -6,4 +6,5 @@ for n in `seq 0 $((NUM-1))`
 do
   scp tianlins@jacob.stanford.edu:~/scr/swipe/state/execs/$1/$n.exec/record $1/record-$n
   scp tianlins@jacob.stanford.edu:~/scr/swipe/state/execs/$1/$n.exec/hist $1/hist-$n
+  scp tianlins@jacob.stanford.edu:~/scr/swipe/state/execs/$1/$n.exec/example $1/example-$n
 done
